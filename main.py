d307a79c216b31d42d41e562cb8a23234acd7450
Sanni <saso.honkanen@gmail.com>
2024-04-20 15:33:28 +0300

    Added CORS

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import *
 
 app = FastAPI()
 
+# CORS, allows requests from any origin
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(players.router)
